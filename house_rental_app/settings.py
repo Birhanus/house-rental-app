a4d9e21012679e95d07c4bf7a6d775fccf814c77
Birhanus <88324419+Birhanus@users.noreply.github.com>
2022-07-07 02:58:38 +0300

    Fix

--- house_rental_app/settings.py
+++ house_rental_app/settings.py
@@ -146,5 +146,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'birhanushimles@gmail.com'
-EMAIL_HOST_PASSWORD = 'f0928127613ff'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'pass'
